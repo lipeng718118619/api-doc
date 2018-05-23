0062bf4c27468ffd25886be2114bb46a51c5c749
lipeng <lipengab@chanjet.com>
2018-05-23 18:01:43 +0800

    代码上传

---
+++ src/main/java/com/chanjet/apidoc/apidoc/ApiDocApplication.java
@@ -0,0 +1,12 @@
+package com.chanjet.apidoc.apidoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDocApplication.class, args);
+	}
+}
